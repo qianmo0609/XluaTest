f1ed622eb98a0c36c430e72618345bbfbe5fae9d
qianmo <3103768332@qq.com>
2025-01-06 01:26:30 +0800

    增加C#调用Lua代码测试

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,23 +1,153 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using XLua;
 
 public class Test : MonoBehaviour
 {
+    LuaEnv luaenv = null;
+
     void Start()
     {
-        LuaEnv luaenv = new LuaEnv();
-        luaenv.DoString("CS.UnityEngine.Debug.Log('hello world')");
+        luaenv = new LuaEnv();
+        luaenv.AddLoader(customLoader);
+        luaenv.DoString("require 'main'");
+        //this.GetGlobal();
+        //this.GetClassOrStruct();
+        //this.GetInterface();
+        //this.GetCollections();
+        //this.GetLuaTable();
+        this.GetLuaFuncDelegate();
         luaenv.Dispose();
     }
 
-    private void Update()
+    public byte[] customLoader(ref string filepath)
     {
-        /*if (Input.GetButtonDown("Submit"))
-        {
-            Debug.Log("AAAAAAAAAA");
-        }*/
+        /*加载Lua代码*/
+#if UNITY_EDITOR
+        return AssetDatabase.LoadAssetAtPath<TextAsset>($"Assets/Lua/{filepath}.lua.txt").bytes;
+#endif
+        return null;
     }
+
+    #region C#callLua
+
+    public void GetGlobal()
+    {
+        int hp = luaenv.Global.Get<int>("hp");
+        bool isPlay = luaenv.Global.Get<bool>("isPlay");
+        string heroName = luaenv.Global.Get<string>("heroName");
+        Debug.Log($"HP:{hp},isPlay:{isPlay},heroName:{heroName}");
+    }
+
+    public class PlayerInfo
+    {
+        public int id;
+        public string name;
+        public int level;
+    }
+
+    public struct EventMsg
+    {
+        public int eventId;
+        public string param;
+    }
+
+    public void GetClassOrStruct()
+    {
+        //映射到有对应字段的class，by value
+        PlayerInfo info = luaenv.Global.Get<PlayerInfo>("playerInfo");
+        EventMsg msg = luaenv.Global.Get<EventMsg>("eventMsg");
+        Debug.Log($"PlayerInfo:{info.id},{info.name},{info.level}");
+        Debug.Log($"EventMsg:{msg.eventId},{msg.param}");
+    }
+
+    [CSharpCallLua]
+    public interface IPlayerPosition
+    {
+        int x { get; set; }
+        int y { get; set; }
+        int z { get; set; }
+
+        void add(int x, int y, int z);
+        void sub(int x,int y ,int z);
+    }
+
+    public void GetInterface()
+    {
+        //映射到interface实例，by ref，这个要求interface加到生成列表，否则会返回null，建议用法
+        IPlayerPosition pPos = luaenv.Global.Get<IPlayerPosition>("playerPosition");
+        Debug.Log($"{pPos.x}，{pPos.y}，{pPos.z}");
+        pPos.add(10,1,23);
+        Debug.Log($"{pPos.x}，{pPos.y}，{pPos.z}");
+        pPos.sub(1,-1,11);
+        Debug.Log($"{pPos.x}，{pPos.y}，{pPos.z}");
+    }
+
+    public void GetCollections()
+    {
+        //映射到Dictionary<string, double>，by value
+        Dictionary<string, double> d = luaenv.Global.Get<Dictionary<string, double>>("Item");
+        Debug.Log(d.Count);
+
+        //映射到List<double>，by value
+        List<double> l = luaenv.Global.Get<List<double>>("Item"); 
+        Debug.Log(l.Count);
+    }
+
+    [CSharpCallLua]
+    public delegate void AddMethod(LuaTable self,int x,int y, int z);
+
+    [CSharpCallLua]
+    public delegate Action addAc(LuaTable t, int x, int y, int z);
+
+
+    public void GetLuaTable()
+    {
+        //映射到LuaTable，by ref
+        LuaTable info= luaenv.Global.Get<LuaTable>("playerPosition");
+        /*var LF = info.Get<LuaFunction>("add");
+        LF.Call(info,1,2,3);
+        Debug.Log($"playerPosition ：{info.Get<int>("x")}，{info.Get<int>("y")}，{info.Get<int>("z")}");*/
+
+        AddMethod LD = info.Get<AddMethod>("add");
+        LD(info,2, 3, 4);
+        Debug.Log($"playerPosition ：{info.Get<int>("x")}，{info.Get<int>("y")}，{info.Get<int>("z")}");
+
+        var ac = info.Get<Action<LuaTable,int,int,int>>("add");
+        ac(info, 2, 3, 4);
+        Debug.Log($"playerPosition ：{info.Get<int>("x")}，{info.Get<int>("y")}，{info.Get<int>("z")}");
+
+        var aac = info.Get<addAc>("add");
+        aac(info, 2, 3, 4);
+        Debug.Log($"playerPosition ：{info.Get<int>("x")}，{info.Get<int>("y")}，{info.Get<int>("z")}");
+    }
+
+
+    [CSharpCallLua]
+    public delegate void test1(int x);
+
+    [CSharpCallLua]
+    public delegate Action test2(int x);
+
+    public void GetLuaFuncDelegate()
+    {
+        var lf = luaenv.Global.Get<LuaFunction>("test");
+        lf.Call(10);
+
+        /*test1 LD = luaenv.Global.Get<test1>("test");
+        LD(100);
+
+        var ac = luaenv.Global.Get<Action<int>>("test");
+        ac(0);
+
+        var aac = luaenv.Global.Get<test2>("test");
+        aac(19);*/
+    }
+
+    #endregion
 }
---
+++ Assets/XLua/Gen/Test_IPlayerPositionBridge.cs
@@ -0,0 +1,269 @@
+﻿#if USE_UNI_LUA
+using LuaAPI = UniLua.Lua;
+using RealStatePtr = UniLua.ILuaState;
+using LuaCSFunction = UniLua.CSharpFunctionDelegate;
+#else
+using LuaAPI = XLua.LuaDLL.Lua;
+using RealStatePtr = System.IntPtr;
+using LuaCSFunction = XLua.LuaDLL.lua_CSFunction;
+#endif
+
+using XLua;
+using System;
+
+
+namespace XLua.CSObjectWrap
+{
+    public class TestIPlayerPositionBridge : LuaBase, Test.IPlayerPosition
+    {
+	    public static LuaBase __Create(int reference, LuaEnv luaenv)
+		{
+		    return new TestIPlayerPositionBridge(reference, luaenv);
+		}
+		
+		public TestIPlayerPositionBridge(int reference, LuaEnv luaenv) : base(reference, luaenv)
+        {
+        }
+		
+        
+		void Test.IPlayerPosition.add(int x, int y, int z)
+		{
+#if THREAD_SAFE || HOTFIX_ENABLE
+            lock (luaEnv.luaEnvLock)
+            {
+#endif
+				RealStatePtr L = luaEnv.L;
+				int err_func = LuaAPI.load_error_func(L, luaEnv.errorFuncRef);
+				
+				
+				LuaAPI.lua_getref(L, luaReference);
+				LuaAPI.xlua_pushasciistring(L, "add");
+				if (0 != LuaAPI.xlua_pgettable(L, -2))
+				{
+					luaEnv.ThrowExceptionFromError(err_func - 1);
+				}
+				if(!LuaAPI.lua_isfunction(L, -1))
+				{
+					LuaAPI.xlua_pushasciistring(L, "no such function add");
+					luaEnv.ThrowExceptionFromError(err_func - 1);
+				}
+				LuaAPI.lua_pushvalue(L, -2);
+				LuaAPI.lua_remove(L, -3);
+				LuaAPI.xlua_pushinteger(L, x);
+				LuaAPI.xlua_pushinteger(L, y);
+				LuaAPI.xlua_pushinteger(L, z);
+				
+				int __gen_error = LuaAPI.lua_pcall(L, 4, 0, err_func);
+				if (__gen_error != 0)
+					luaEnv.ThrowExceptionFromError(err_func - 1);
+				
+				
+				
+				LuaAPI.lua_settop(L, err_func - 1);
+				
+#if THREAD_SAFE || HOTFIX_ENABLE
+            }
+#endif
+		}
+        
+		void Test.IPlayerPosition.sub(int x, int y, int z)
+		{
+#if THREAD_SAFE || HOTFIX_ENABLE
+            lock (luaEnv.luaEnvLock)
+            {
+#endif
+				RealStatePtr L = luaEnv.L;
+				int err_func = LuaAPI.load_error_func(L, luaEnv.errorFuncRef);
+				
+				
+				LuaAPI.lua_getref(L, luaReference);
+				LuaAPI.xlua_pushasciistring(L, "sub");
+				if (0 != LuaAPI.xlua_pgettable(L, -2))
+				{
+					luaEnv.ThrowExceptionFromError(err_func - 1);
+				}
+				if(!LuaAPI.lua_isfunction(L, -1))
+				{
+					LuaAPI.xlua_pushasciistring(L, "no such function sub");
+					luaEnv.ThrowExceptionFromError(err_func - 1);
+				}
+				LuaAPI.lua_pushvalue(L, -2);
+				LuaAPI.lua_remove(L, -3);
+				LuaAPI.xlua_pushinteger(L, x);
+				LuaAPI.xlua_pushinteger(L, y);
+				LuaAPI.xlua_pushinteger(L, z);
+				
+				int __gen_error = LuaAPI.lua_pcall(L, 4, 0, err_func);
+				if (__gen_error != 0)
+					luaEnv.ThrowExceptionFromError(err_func - 1);
+				
+				
+				
+				LuaAPI.lua_settop(L, err_func - 1);
+				
+#if THREAD_SAFE || HOTFIX_ENABLE
+            }
+#endif
+		}
+        
+
+        
+        int Test.IPlayerPosition.x 
+        {
+            
+            get 
+            {
+#if THREAD_SAFE || HOTFIX_ENABLE
+                lock (luaEnv.luaEnvLock)
+                {
+#endif
+					RealStatePtr L = luaEnv.L;
+					int oldTop = LuaAPI.lua_gettop(L);
+					
+					LuaAPI.lua_getref(L, luaReference);
+					LuaAPI.xlua_pushasciistring(L, "x");
+					if (0 != LuaAPI.xlua_pgettable(L, -2))
+					{
+						luaEnv.ThrowExceptionFromError(oldTop);
+					}
+					int __gen_ret = LuaAPI.xlua_tointeger(L, -1);
+					LuaAPI.lua_pop(L, 2);
+					return __gen_ret;
+#if THREAD_SAFE || HOTFIX_ENABLE
+                }
+#endif
+            }
+            
+            
+            set
+            {
+#if THREAD_SAFE || HOTFIX_ENABLE
+                lock (luaEnv.luaEnvLock)
+                {
+#endif
+					RealStatePtr L = luaEnv.L;
+					int oldTop = LuaAPI.lua_gettop(L);
+					
+					LuaAPI.lua_getref(L, luaReference);
+					LuaAPI.xlua_pushasciistring(L, "x");
+					LuaAPI.xlua_pushinteger(L, value);
+					if (0 != LuaAPI.xlua_psettable(L, -3))
+					{
+						luaEnv.ThrowExceptionFromError(oldTop);
+					}
+					LuaAPI.lua_pop(L, 1);
+#if THREAD_SAFE || HOTFIX_ENABLE
+                }
+#endif
+            }
+            
+        }
+        
+        int Test.IPlayerPosition.y 
+        {
+            
+            get 
+            {
+#if THREAD_SAFE || HOTFIX_ENABLE
+                lock (luaEnv.luaEnvLock)
+                {
+#endif
+					RealStatePtr L = luaEnv.L;
+					int oldTop = LuaAPI.lua_gettop(L);
+					
+					LuaAPI.lua_getref(L, luaReference);
+					LuaAPI.xlua_pushasciistring(L, "y");
+					if (0 != LuaAPI.xlua_pgettable(L, -2))
+					{
+						luaEnv.ThrowExceptionFromError(oldTop);
+					}
+					int __gen_ret = LuaAPI.xlua_tointeger(L, -1);
+					LuaAPI.lua_pop(L, 2);
+					return __gen_ret;
+#if THREAD_SAFE || HOTFIX_ENABLE
+                }
+#endif
+            }
+            
+            
+            set
+            {
+#if THREAD_SAFE || HOTFIX_ENABLE
+                lock (luaEnv.luaEnvLock)
+                {
+#endif
+					RealStatePtr L = luaEnv.L;
+					int oldTop = LuaAPI.lua_gettop(L);
+					
+					LuaAPI.lua_getref(L, luaReference);
+					LuaAPI.xlua_pushasciistring(L, "y");
+					LuaAPI.xlua_pushinteger(L, value);
+					if (0 != LuaAPI.xlua_psettable(L, -3))
+					{
+						luaEnv.ThrowExceptionFromError(oldTop);
+					}
+					LuaAPI.lua_pop(L, 1);
+#if THREAD_SAFE || HOTFIX_ENABLE
+                }
+#endif
+            }
+            
+        }
+        
+        int Test.IPlayerPosition.z 
+        {
+            
+            get 
+            {
+#if THREAD_SAFE || HOTFIX_ENABLE
+                lock (luaEnv.luaEnvLock)
+                {
+#endif
+					RealStatePtr L = luaEnv.L;
+					int oldTop = LuaAPI.lua_gettop(L);
+					
+					LuaAPI.lua_getref(L, luaReference);
+					LuaAPI.xlua_pushasciistring(L, "z");
+					if (0 != LuaAPI.xlua_pgettable(L, -2))
+					{
+						luaEnv.ThrowExceptionFromError(oldTop);
+					}
+					int __gen_ret = LuaAPI.xlua_tointeger(L, -1);
+					LuaAPI.lua_pop(L, 2);
+					return __gen_ret;
+#if THREAD_SAFE || HOTFIX_ENABLE
+                }
+#endif
+            }
+            
+            
+            set
+            {
+#if THREAD_SAFE || HOTFIX_ENABLE
+                lock (luaEnv.luaEnvLock)
+                {
+#endif
+					RealStatePtr L = luaEnv.L;
+					int oldTop = LuaAPI.lua_gettop(L);
+					
+					LuaAPI.lua_getref(L, luaReference);
+					LuaAPI.xlua_pushasciistring(L, "z");
+					LuaAPI.xlua_pushinteger(L, value);
+					if (0 != LuaAPI.xlua_psettable(L, -3))
+					{
+						luaEnv.ThrowExceptionFromError(oldTop);
+					}
+					LuaAPI.lua_pop(L, 1);
+#if THREAD_SAFE || HOTFIX_ENABLE
+                }
+#endif
+            }
+            
+        }
+        
+        
+        
+		
+		
+	}
+}
